0ebb3d9415dd59281d8b4300263fc72b9f171e05
Ahemd Shehata <a.shehata.cs@gmail.com>
2024-09-16 13:19:37 +0300

    Switch from createBrowserRouter to createHashRouter for GitHub Pages compatibility - Changed routing setup from createBrowserRouter to createHashRouter - Updated routes to use hash-based routing to handle client-side routing on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
 import LoginPage from "./Components/LoginPage/LoginPage";
-import { createBrowserRouter } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./Components/MainPage/MainPage";
 import Layout from "./Components/Layout/Layout";
 import store from "./Components/Redux/store";
 import { Provider } from "react-redux";
-import TaskCard from "./Components/TaskCard/TaskCard";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <Layout />,
@@ -21,11 +19,9 @@ function App() {
   ]);
 
   return (
-    <>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   );
 }
 
